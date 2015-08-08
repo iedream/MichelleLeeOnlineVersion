1c10c5210fc4b030e70c9bc6a75b7717e889c0f1
Catherine Zhao <iecatherine@hotmail.ca>
2015-08-07 21:23:35 -0400

    Initial Commit

---
+++ Michelle Lee Collection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Michelle Lee Collection
+//
+//  Created by Catherine Zhao on 2015-08-07.
+//  Copyright © 2015 Catherine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
